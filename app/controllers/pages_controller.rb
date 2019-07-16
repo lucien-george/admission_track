7e2a188af1a35b5aa4cbb32534c458100c802a24
Lucien George <luciengeorge95@gmail.com>
2019-07-16 15:02:08 +0100

    search + top rated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
+require 'open-uri'
+require 'json'
+
 class PagesController < ApplicationController
   def home
+    if params[:movie].present?
+      @movies = JSON.parse(open("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{params[:movie]}").read)['results']
+    else
+      @movies = JSON.parse(open("https://api.themoviedb.org/3/movie/upcoming?api_key=#{ENV['TMDB_API_KEY']}").read)['results']
+    end
+    @top_rated = JSON.parse(open("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}").read)['results'].first(4)
   end
 end
